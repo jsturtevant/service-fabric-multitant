92ec7cf471fbc8e6b53d20e3cd7d8a83fb753442
Vaclav Turecek <vaclav@live.com>
2016-07-27 21:31:09 -0700

    adding script

---
+++ src/demo.ps1
@@ -0,0 +1,7 @@
+﻿Connect-ServiceFabricCluster
+
+Copy-ServiceFabricApplicationPackage -ApplicationPackagePath "C:\gitvturecek\job-creator-demo\src\JobCreatorDemo\pkg\Debug" -ImageStoreConnectionString "file:C:\SfDevCluster\Data\ImageStoreShare" -ApplicationPackagePathInImageStore "JobCreatorDemo"
+
+Register-ServiceFabricApplicationType -ApplicationPathInImageStore "JobCreatorDemo"
+
+New-ServiceFabricApplication -ApplicationName "fabric:/Demo2" -ApplicationTypeName "JobCreatorDemoType" -ApplicationTypeVersion "1.0.0" -ApplicationParameter @{"WebService_AppPath" = "demo2"; }
